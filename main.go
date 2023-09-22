a00ca73e32bbbfe75491f0a82df4508e68ed5d7c
Cedric K. Wachira <cedric.kiama@gmail.com>
2023-09-22 13:36:22 +0400

    I have added new lines of code  for the generic SumNumbers function, beneath the functions I already have. It declares a generic function with the same logic as the generic function I declared previously, but with the new interface type instead of the union as the type constraint. As before, I used the type parameters for the argument and return types.

--- main.go
+++ main.go
@@ -60,3 +60,13 @@ func SumIntsOrFloats[K comparable, V int64 | float64](m map[K]V) V {
     }
     return s
 }
+
+// SumNumbers sums the values of map m. It supports both integers
+// and floats as map values.
+func SumNumbers[K comparable, V Number](m map[K]V) V {
+    var s V
+    for _, v := range m {
+        s += v
+    }
+    return s
+}
